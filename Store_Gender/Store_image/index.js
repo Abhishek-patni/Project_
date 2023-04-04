2ca498401662c0278947566b3144158aa75d78de
Abhishek <patniapk1234@gmail.com>
2023-04-04 17:20:35 +0530

    Storage image

---
+++ Store_Gender/Store_image/index.js
@@ -0,0 +1,51 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTuXtTS2MIJv4bW-XnUeeNbtCGVJj0hpo",
+    authDomain: "dating-web-c6ce9.firebaseapp.com",
+    databaseURL: "https://dating-web-c6ce9-default-rtdb.firebaseio.com",
+    projectId: "dating-web-c6ce9",
+    storageBucket: "dating-web-c6ce9.appspot.com",
+    messagingSenderId: "622569066360",
+    appId: "1:622569066360:web:76def5dd380d5ad812428d",
+    measurementId: "G-5HWWGSE4VB"
+  };
+firebase.initializeApp(firebaseConfig);
+
+var fileText = document.querySelector(".fileText");
+var uploadPercentage = document.querySelector(".uploadPercentage");
+var progress =  document.querySelector(".progress");
+var percentVal;
+var fileItem;
+var fileName;
+var img = document.querySelector(".img");
+ function getFile(e){
+    fileItem = e.target.files[0];
+    fileName = fileItem.name;
+    fileText.innerHTML = fileName;
+}
+
+
+function uploadImage(){
+    let storageRef = firebase.storage().ref("images/"+fileName);
+    let uploadTask = storageRef.put(fileItem);
+    uploadTask.on("state_changed",(snapshot)=>{
+        console.log(snapshot);
+        percentVal = Math.floor((snapshot.bytesTransferred/snapshot.totalBytes)*100);
+        console.log(percentVal);
+        uploadPercentage.innerHTML = percentVal+"%";
+        progress.style.width=percentVal+"%";
+    },(error)=>{
+        console.log("Error is ", error);
+    },()=>{
+        uploadTask.snapshot.ref.getDownloadURL().then((url)=>{
+            console.log("URL", url);
+            if(url != ""){
+                img.setAttribute("src",url);
+                img.style.display="block";
+                alert("We got your data your card will be added within Three upcoming days"),
+                setTimeout(function(){
+                    window.location.href = "../reg2.html"; // Redirect to Google after 10 seconds
+                }, 10000);
+            }
+        })
+    })
+}
--- data.js
+++ data.js
@@ -111,3 +111,43 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
+
+  //lets store images 
+
+  var fileText = document.querySelector(".fileText");
+  var uploadPercentage = document.querySelector(".uploadPercentage");
+  var progress =  document.querySelector(".progress");
+  var percentVal;
+  var fileItem;
+  var fileName;
+  var img = document.querySelector(".img");
+   function getFile(e){
+      fileItem = e.target.files[0];
+      fileName = fileItem.name;
+      fileText.innerHTML = fileName;
+  }
+  
+  
+  function uploadImage(){
+      let storageRef = firebase.storage().ref("images/"+fileName);
+      let uploadTask = storageRef.put(fileItem);
+      uploadTask.on("state_changed",(snapshot)=>{
+          console.log(snapshot);
+          percentVal = Math.floor((snapshot.bytesTransferred/snapshot.totalBytes)*100);
+          console.log(percentVal);
+          uploadPercentage.innerHTML = percentVal+"%";
+          progress.style.width=percentVal+"%";
+      },(error)=>{
+          console.log("Error is ", error);
+      },()=>{
+  
+          uploadTask.snapshot.ref.getDownloadURL().then((url)=>{
+              console.log("URL", url);
+  
+              if(url != ""){
+                  img.setAttribute("src",url);
+                  img.style.display="block";
+              }
+          })
+      })
+  }
